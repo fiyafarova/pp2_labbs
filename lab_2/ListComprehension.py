9a169c40877f4ed5d517602041e8c85ed3ef64ec
Sofiya <sooofiy@mail.ru>
2025-01-22 20:58:15 +0500

    add lab2 part

---
+++ lab_2/ListComprehension.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+
+
+
+
+newlist = [x for x in fruits if x != "apple"]
+
+
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
---
+++ lab_2/RemoveListItems.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
